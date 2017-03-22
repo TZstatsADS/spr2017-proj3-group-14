84c128dd9bd079afadce113be22d64c7d8ee0be8
Yi Xiang <yx2365@columbia.edu>
2017-03-22 07:30:51 -0400

    the same, with switch

--- lib/test.R
+++ lib/test.R
@@ -18,7 +18,17 @@ test = function(fit_train, dat_test){
                newdata = dat_test,
                n.trees = fit_train$n.trees,
                type="response")
-
+  
+  ##might be useful
+  # pred = switch(class(fit_train), 
+  #               gbm = predict(fit_train, 
+  #                             newdata = dat_test, 
+  #                             n.trees = fit_train$n.trees, 
+  #                             type="response"),
+  #               
+  #               adv = predict(fit_train, 
+  #                             newdata = dat_test)
+  # )
 
   return(as.numeric(pred> 0.5))
 }

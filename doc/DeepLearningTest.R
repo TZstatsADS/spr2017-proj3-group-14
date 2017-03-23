3e995bfb86a7a7c300fe700abebdea0d0bfc5f40
Virgile Mison <virgilemison@gmail.com>
2017-03-23 18:46:17 -0400

    Update latest DNN model to +77% accuracy

--- doc/DeepLearningTest.R
+++ doc/DeepLearningTest.R
@@ -8,15 +8,28 @@ biocLite("EBImage")
 library("EBImage")
 img_dir <- "../data/raw_images/"
 source("../lib/annex.R")
+
+set.seed(1)
 n <- 2000
 sizeImg <- 50
+rot <- 1
 n_files <- length(list.files(img_dir))
-rand_sample <- sample(1:n_files, n)
-imgs <- matrixfy_imgs(img_dir, rand_sample, n, sizeImg, sizeImg)
-labels <- read.csv("../data/labels.csv", header = T)[rand_sample,1]
+# display(getImage(imgs,10,40))  # to show example image from the matrix imgs
 
-data <- test_train_sep(imgs, labels, 0.33333) 
-train.x <- data[[1]]
+### Load images or re-compile them ###
+
+## load data to imgs
+load("data1rotAugmentation.RData")
+
+## only if recompile the data from raw images
+#rand_sample <- sample(1:n_files, n)
+#imgs <- matrixfy_imgs(img_dir, rand_sample, n, sizeImg, sizeImg, rot=rot)
+#labels <- read.csv("../data/labels.csv", header = T)[rand_sample,1]
+#labels <- rep(labels, each=(1*rot+2))
+
+#data <- test_train_sep(imgs, labels, 0.33333)
+data <- test_train_sep_dataaug(imgs, labels, 0.2, rot)
+train.x <- data[[1]] 
 train.y <- data[[2]]
 test.x <- data[[3]]
 test.y <- data[[4]]
@@ -24,41 +37,68 @@ test.y <- data[[4]]
 # Deep Learning phase
 require(mxnet)
 
+# The only model that gives really good result.
+# Training accuracy of 84.56% and test accuracy 77.42%.
+# Model is saved in DNNbest.RData and will be load as "model" if compile line 52 directly.
+
+# To retrain the model:
 mx.set.seed(2)
-model <- mx.mlp(train.x, train.y, hidden_node=c(100,50,10), out_node=2, activation = "sigmoid", out_activation="softmax",
-                num.round=500, array.batch.size=50, learning.rate=0.07,
-                eval.metric=mx.metric.accuracy, dropout = 0.4, momentum = 0.7, initializer=mx.init.normal(1.5))
-graph.viz(model$symbol)
+model <- mx.mlp(train.x, train.y, hidden_node=c(500,250,100,10), out_node=2, activation = "sigmoid", out_activation="softmax",
+                num.round=750, array.batch.size=150, learning.rate=0.3,
+                eval.metric=mx.metric.accuracy, dropout = 0.70, momentum = 0.7, initializer=mx.init.normal(1.5))
 
 accuracy_model(model, test.x,test.y)
-#########################
-# Test with LeNet model #
-#########################
+save(model, file="DNNbest.RData")
+
+# To predict new images:
+load("DNNbest.RData")
+preds = predict(model, test.x)
+pred.label = max.col(t(preds))-1
+print(table(pred.label, test.y))
+
+#################################################################################################################
+# 77.41% model saved (but overfit to 99% in training accuracy) -> might try to improve it but delete it after   #
+#################################################################################################################
+#model <- mx.mlp(train.x, train.y, hidden_node=c(300,200,50), out_node=2, activation = "sigmoid", out_activation="softmax",
+#                num.round=700, array.batch.size=150, learning.rate=0.15,
+#                eval.metric=mx.metric.accuracy, dropout = 0.7, momentum = 0.7, initializer=mx.init.normal(1.5))
+
+
+########################################
+# Test with LeNet model (Advanced CNN) # -> not concluant, lack of data or computing power to train longer.
+########################################
+
+# parameter dropout (proba of drop)
+pdrop <- 0.4
 
 # input
 data <- mx.symbol.Variable('data')
 # first conv
-conv1 <- mx.symbol.Convolution(data=data, kernel=c(5,5), num_filter=10)
-tanh1 <- mx.symbol.Activation(data=conv1, act_type="sigmoid")
+conv1 <- mx.symbol.Convolution(data=data, kernel=c(3,3), num_filter=4)
+tanh1 <- mx.symbol.Activation(data=conv1, act_type="relu")
 pool1 <- mx.symbol.Pooling(data=tanh1, pool_type="max",
                            kernel=c(2,2), stride=c(2,2))
 # second conv
-conv2 <- mx.symbol.Convolution(data=pool1, kernel=c(3,3), num_filter=20)
-tanh2 <- mx.symbol.Activation(data=conv2, act_type="sigmoid")
+conv2 <- mx.symbol.Convolution(data=pool1, kernel=c(2,2), num_filter=8)
+tanh2 <- mx.symbol.Activation(data=conv2, act_type="relu")
 pool2 <- mx.symbol.Pooling(data=tanh2, pool_type="max",
                            kernel=c(2,2), stride=c(2,2))
 # first fullc
 flatten <- mx.symbol.Flatten(data=pool2)
-fc1 <- mx.symbol.FullyConnected(data=flatten, num_hidden=200)
-tanh3 <- mx.symbol.Activation(data=fc1, act_type="tanh")
+drop1 <- mx.symbol.Dropout(flatten,p=pdrop)
+fc1 <- mx.symbol.FullyConnected(data=drop1, num_hidden=250)
+tanh3 <- mx.symbol.Activation(data=fc1, act_type="relu")
+
 # second fullc
-fc2 <- mx.symbol.FullyConnected(data=tanh3, num_hidden=50)
+drop2 <- mx.symbol.Dropout(tanh3, p=pdrop)
+fc2 <- mx.symbol.FullyConnected(data=drop2, num_hidden=100)
+
 # loss
 lenet <- mx.symbol.SoftmaxOutput(data=fc2)
 
-train.array <- train.x
+train.array <- (train.x-apply(train.x,2,mean))/apply(train.x,2,sd)
 dim(train.array) <- c(sizeImg, sizeImg, 1, nrow(train.x))
-test.array <- test.x
+test.array <- (test.x-apply(train.x,2,mean))/apply(train.x,2,sd)
 dim(test.array) <- c(sizeImg, sizeImg, 1, nrow(test.x))
 
 device.cpu <- mx.cpu()
@@ -66,10 +106,157 @@ device.cpu <- mx.cpu()
 mx.set.seed(2)
 tic <- proc.time()
 modelLeNet <- mx.model.FeedForward.create(lenet, X=train.array, y=train.y,
-                                     ctx=device.cpu, num.round=15, array.batch.size=10,
-                                     learning.rate=0.05, optimizer="sgd",
-                                     eval.metric=mx.metric.accuracy, initializer=mx.init.normal(0.2),
-                                     epoch.end.callback=mx.callback.log.train.metric(100))
-                                     # wd=0.00001
-graph.viz(modelLeNet$symbol)
+                                     ctx=device.cpu, num.round=3, array.batch.size=200,
+                                     learning.rate=0.1, optimizer="adagrad",
+                                     eval.metric=mx.metric.accuracy, initializer=mx.init.uniform(1/sizeImg),
+                                     epoch.end.callback=mx.callback.log.train.metric(100))# wd=0.00001)
+
+
 accuracy_model(modelLeNet, test.array,test.y)
+graph.viz(modelLeNet$symbol)
+
+##################
+##  Simple CNN  ## -> not concluant either
+##################
+
+pdrop <- 0.7
+
+# input
+data <- mx.symbol.Variable('data')
+# first conv
+conv1 <- mx.symbol.Convolution(data=data, kernel=c(3,3), num_filter=10)
+tanh1 <- mx.symbol.Activation(data=conv1, act_type="relu")
+pool1 <- mx.symbol.Pooling(data=tanh1, pool_type="max",
+                           kernel=c(2,2), stride=c(2,2))
+
+# second conv
+conv2 <- mx.symbol.Convolution(data=pool1, kernel=c(2,2), num_filter=15)
+tanh2 <- mx.symbol.Activation(data=conv2, act_type="relu")
+pool2 <- mx.symbol.Pooling(data=tanh2, pool_type="max",
+                           kernel=c(2,2), stride=c(2,2))
+
+# first fullc
+flatten <- mx.symbol.Flatten(data=pool2)
+drop1 <- mx.symbol.Dropout(flatten,p=pdrop)
+fc1 <- mx.symbol.FullyConnected(data=drop1, num_hidden=200)
+relu1 <- mx.symbol.Activation(data=fc1, act_type="relu")
+
+# loss
+output <- mx.symbol.SoftmaxOutput(data=relu1)
+
+train.array <- (train.x-apply(train.x,2,mean))/apply(train.x,2,sd)
+dim(train.array) <- c(sizeImg, sizeImg, 1, nrow(train.x))
+test.array <- (test.x-apply(train.x,2,mean))/apply(train.x,2,sd)
+dim(test.array) <- c(sizeImg, sizeImg, 1, nrow(test.x))
+
+device.cpu <- mx.cpu()
+
+mx.set.seed(0)
+tic <- proc.time()
+modelCNN <- mx.model.FeedForward.create(output, X=train.array, y=train.y,
+                                          ctx=device.cpu, num.round=50, array.batch.size=250,
+                                          learning.rate=0.5, optimizer="adagrad",
+                                          eval.metric=mx.metric.accuracy, initializer=mx.init.uniform(1/sizeImg),
+                                          epoch.end.callback=mx.callback.log.train.metric(150))# wd=0.00001)
+accuracy_model(modelCNN, test.array,test.y)
+
+
+# good links to understand CNN:
+# CNN: http://web.engr.illinois.edu/~slazebni/spring14/lec24_cnn.pdf
+# optimizer in NN: http://sebastianruder.com/optimizing-gradient-descent/
+# set up CNN : http://cs231n.github.io/neural-networks-2/#init
+# initialization weights CNN: https://github.com/NVIDIA/DIGITS/blob/master/examples/weight-init/README.md
+# on LeNet: http://deeplearning.net/tutorial/deeplearning.pdf
+
+############## -> applied directly on image matrices
+##  t-SNE  ### -> separation of classes does not occur. The decorrelation does not work.
+############## -> maybe because of similarities in the images and other element not relevant (background)
+
+library("Rtsne")
+
+colors = c("red","blue")
+names(colors) = unique(train.y)
+
+## Executing the algorithm on curated data for 2D
+tsne <- Rtsne(train.x, dims = 2, perplexity=20, initial_dims=30, check_duplicates=F, theta=0.3, verbose=TRUE, max_iter = 500)
+
+## Plotting 2D
+plot(tsne$Y, t='n', main="tsne")
+text(tsne$Y, labels=train.y, col=colors[train.y+1])
+
+## Executing the algorithm on curated data for 3D
+tsne <- Rtsne(train.x, dims = 3, perplexity=20, initial_dims=30, check_duplicates=F, theta=0.3, verbose=TRUE, max_iter = 500)
+
+## Plotting 3D
+library("scatterplot3d")
+scatterplot3d(tsne$Y[,1],tsne$Y[,2],tsne$Y[,3],color=colors[train.y+1])
+
+###################
+## Sift features ## -> DNN does not work, diverges
+###################
+
+siftF <- t(apply(read.csv("../data/sift_features.csv", header=F),2,as.numeric))
+#siftF <- t(read.csv("../data/sift_features.csv", header = T))
+labels <- read.csv("../data/labels.csv", header = T)[,1]
+
+dataSift <- test_train_sep(siftF, labels, 0.33333) 
+trainS.x <- dataSift[[1]]
+trainS.y <- dataSift[[2]]
+testS.x <- dataSift[[3]]
+testS.y <- dataSift[[4]]
+
+mx.set.seed(2)
+modelS <- mx.mlp(trainS.x, trainS.y, hidden_node=c(300,500,200,100), out_node=2, activation = "sigmoid", out_activation="softmax",
+                num.round=50, array.batch.size=50, learning.rate=0.1,
+                eval.metric=mx.metric.accuracy, dropout = 0.6, momentum = 0.7, initializer=mx.init.normal(1.5))
+
+accuracy_model(modelS, testS.x,testS.y)
+graph.viz(modelS$symbol)
+
+############## -> applied directly on sift features
+##  t-SNE  ### -> separation of classes does not occur. The decorrelation does not work.
+############## 
+
+colors = c("red","blue")
+names(colors) = unique(train.y)
+
+## Executing the algorithm on curated data
+tsne <- Rtsne(trainS.x, dims = 2, perplexity=20, initial_dims=50, check_duplicates=F, theta=0.1, verbose=TRUE, max_iter = 700)
+#exeTimeTsne<- system.time(Rtsne(train.x, dims = 2, perplexity=30, verbose=TRUE, max_iter = 500))
+
+## Plotting 2D
+plot(tsne$Y, t='n', main="tsne")
+text(tsne$Y, labels=trainS.y, col=colors[trainS.y+1])
+
+#####################
+## Lasso features  ## -> DNN diverges to 50-50% classification
+#####################
+
+lasso <- apply(read.csv("../data/fea_lasso.csv", header=F),2,as.numeric)
+
+dataLasso <- test_train_sep(lasso, labels, 0.33333) 
+trainL.x <- dataLasso[[1]]
+trainL.y <- dataLasso[[2]]
+testL.x <- dataLasso[[3]]
+testL.y <- dataLasso[[4]]
+
+mx.set.seed(2)
+modelL <- mx.mlp(trainL.x, trainL.y, hidden_node=c(1000,200,200,50), out_node=2, activation = "relu", out_activation="softmax",
+                num.round=100, array.batch.size=100, learning.rate=0.05,
+                eval.metric=mx.metric.accuracy, dropout = 0.4, momentum=0.7, initializer=mx.init.normal(0.05))
+accuracy_model(modelL, testL.x,testL.y)
+
+############## -> applied directly on lasso features
+##  t-SNE  ### -> separation of classes does not occur. The decorrelation does not work.
+############## 
+
+colors = c("red","blue")
+names(colors) = unique(train.y)
+
+## Executing the algorithm on curated data
+tsne <- Rtsne(trainL.x, dims = 2, perplexity=20, initial_dims=50, check_duplicates=F, theta=0.1, verbose=TRUE, max_iter = 700)
+#exeTimeTsne<- system.time(Rtsne(train.x, dims = 2, perplexity=30, verbose=TRUE, max_iter = 500))
+
+## Plotting 2D
+plot(tsne$Y, t='n', main="tsne")
+text(tsne$Y, labels=trainL.y, col=colors[trainL.y+1])
--- lib/annex.R
+++ lib/annex.R
@@ -14,7 +14,7 @@ crop_image_ratio11 <- function(img){
 }
 
 
-matrixfy_imgs <- function(img_dir, rand_sample, n=NA, width=100 ,height=100){
+matrixfy_imgs <- function(img_dir, rand_sample, n=NA, width=100 ,height=100, rot=0){
   
   ### Convert images to matrices for training/testing images
 
@@ -24,18 +24,46 @@ matrixfy_imgs <- function(img_dir, rand_sample, n=NA, width=100 ,height=100){
   ### load libraries
   library("EBImage")
   n_files <- length(list.files(img_dir))
-  
-  ### store vectorized pixel values of images
-  dat <- matrix(NA, n, width*height)
-  cpt<-1
-  for(i in rand_sample){
-    # name of image 1: image_0001
-    nb <- paste(c(rep("0",3-floor(log(i,10))),i),collapse="")
-    img <- readImage(paste0(img_dir,  "image", "_", nb,".jpg"))
-    dat[cpt,] <- as.vector(resize(crop_image_ratio11(img), width, height))
-    cpt<-cpt+1
+  if (rot==0) {
+    ### store vectorized pixel values of images
+    dat <- matrix(NA, n, width*height)
+    cpt<-1
+    for(i in rand_sample){
+      # name of image 1: image_0001
+      nb <- paste(c(rep("0",3-floor(log(i,10))),i),collapse="")
+      img <- readImage(paste0(img_dir,  "image", "_", nb,".jpg"))
+      dat[cpt,] <- as.vector(resize(crop_image_ratio11(img), width, height))
+      cpt<-cpt+1
+    }
+  }
+  else {
+    dat <- matrix(NA, (1*rot+2)*n, width*height)
+    cpt<-1
+    for(i in rand_sample){
+      # name of image 1: image_0001
+      nb <- paste(c(rep("0",3-floor(log(i,10))),i),collapse="")
+      img <- readImage(paste0(img_dir,  "image", "_", nb,".jpg"))
+      img_aug <- data_aug.rotation(img, rot)
+      for (j in 1:(1*rot+2)) {
+        dat[cpt,] <- as.vector(resize(crop_image_ratio11(img_aug[[j]]), width, height))
+        cpt<-cpt+1
+      }
+    }
   }
   return(dat)
+}
+
+data_aug.rotation <- function(img, times) { # implement also symetry
+  angles <- seq(0,5,length.out = times+1)[1:times+1]
+  res <- vector("list", times+2)
+  res[[1]] <- img
+  res[[2]] <- flop(img)
+  counter <- 3
+  for (angle in angles) {
+    res[[counter]] <- rotate(img, angle)
+    counter <- counter+1
+  }
+  return(res)
 }
 
 getImage <- function(imgs, index, width){
@@ -49,13 +77,21 @@ test_train_sep <- function(data, labels, test_perc){
   n <- nrow(data)
   n_th <- floor(n*test_perc)
   rand_sample <- sample(1:n,n_th)
-  return(list(imgs[-rand_sample,], labels[-rand_sample],imgs[rand_sample,], labels[rand_sample]))
+  return(list(data[-rand_sample,], labels[-rand_sample],data[rand_sample,], labels[rand_sample]))
+}
+
+test_train_sep_dataaug <- function(data, labels, test_perc, rot){
+  # return a list with train,test sets
+  n <- nrow(data)/(1*rot+2)
+  n_th <- floor(n*test_perc)
+  rand_sample <- sample(1:n,n_th)
+  s <- rep((rand_sample-1)*(1*rot+2),each=1*rot+2)+(1:(1*rot+2))
+  return(list(data[-s,], labels[-s],data[s,], labels[s]))
 }
 
 accuracy_model <- function(model, test.x, test.y){
   preds = predict(model, test.x)
   pred.label = max.col(t(preds))-1
-  print(mean(pred.label))
   print(table(pred.label, test.y))
-  sprintf("Accuracy of model: %s%%",round(mean(pred.label==test.y),3)) 
+  sprintf("Accuracy of model: %s%%",round(mean(pred.label==test.y)*100,3)) 
 }
